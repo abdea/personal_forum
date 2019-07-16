bb023dbd71b37d632dc97d4d876d714359e1b74e
abdea <821363696@qq.com>
2019-07-16 22:30:17 +0800

    add mysql

---
+++ server/sql.js
@@ -0,0 +1,15 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '54yangrui.',
+  database : 'sakila'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
