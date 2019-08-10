ce03a315eb98acdc94331407cdf8516dbc3872dd
abdea <821363696@qq.com>
2019-08-10 20:36:28 +0800

    add backgroud from vue-element-admin

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  resolve: {
+    alias: {
+      view: path.resolve(__dirname, 'src/view/'),
+      components: path.resolve(__dirname, 'src/components/')
+    }
+  }
 }
